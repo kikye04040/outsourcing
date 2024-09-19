d0a68b4a8ee6f241820c4a4418588b5cd2ce31f0
Jay3029 <lttleleepp@gmail.com>
2024-09-19 14:47:44 +0900

    리뷰 기본틀 완성 다른 부분들과의 연관관계 설정이 안되어 있음

---
+++ src/main/java/com/sparta/outsourcing/review/reviewDTO/ReviewRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.outsourcing.review.reviewDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewRequestDto {
+
+    private Long reviewId;
+    private Long orderId;
+    private int rating;
+    private String contents;
+    private String reviewPictureUrl;
+}
