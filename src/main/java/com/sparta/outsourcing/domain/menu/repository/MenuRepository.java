9ecf9ef07e52a0973f5cd5ba7d29afd493348b76
EtherXion <etel0103@gmail.com>
2024-09-20 12:34:17 +0900

    fix menu repository pull 과정에서 날아간 코드 수정

--- src/main/java/com/sparta/outsourcing/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/repository/MenuRepository.java
@@ -2,10 +2,19 @@ package com.sparta.outsourcing.domain.menu.repository;
 
 import com.sparta.outsourcing.domain.menu.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("SELECT c FROM Menu c JOIN FETCH c.store WHERE c.store.storeId = :storeId AND c.deleted = false")
+    List<Menu> findByStoreId(@Param("storeId") Long storeId);
+
+    Optional<Menu> findByMenuId(Long menuId);
+
     // 메뉴 이름으로 메뉴 찾기 추가 (기혜)
     Optional<Menu> findByName(String menuName);
 }
