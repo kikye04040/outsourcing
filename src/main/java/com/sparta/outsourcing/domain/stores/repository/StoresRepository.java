2a5a08cc508137bcfd216478ae054f06cf49765f
Hyemin Yang <asitwas729@gmail.com>
2024-09-20 14:41:38 +0900

    fix: 사용자가 만든 가게 개수 카운트 기능 추가

--- src/main/java/com/sparta/outsourcing/domain/stores/repository/StoresRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/stores/repository/StoresRepository.java
@@ -4,7 +4,13 @@ import com.sparta.outsourcing.domain.stores.entity.Stores;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoresRepository extends JpaRepository<Stores, Long> {
     Page<Stores> findByNameContaining(String keyword, Pageable pageable);
+
+    // 특정 사용자 ID로 가게 개수 카운트
+    @Query("SELECT COUNT(s) FROM Stores s WHERE s.user.id = :userId")
+    int countByUserId(@Param("userId") Long userId);
 }
