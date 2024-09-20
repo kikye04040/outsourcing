6ec501b7a88f4d6573be31fe53931204699786a0
golden-hamster <isack19999@gmail.com>
2024-09-20 16:54:20 +0900

    feat: 사용자 조회 구현

--- src/main/java/com/sparta/outsourcing/domain/user/dto/response/UserResponse.java
+++ src/main/java/com/sparta/outsourcing/domain/user/dto/response/UserResponse.java
@@ -6,17 +6,27 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Builder(access = AccessLevel.PROTECTED)
 @Getter
 public class UserResponse {
     private String email;
     private String createdDate;
+    private String name;
+    private String currentAddress;
+    private String phone;
+    private String role;
 
     public static UserResponse from(User user) {
         return UserResponse.builder()
                 .email(user.getEmail())
                 .createdDate(user.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분")))
+                .name(user.getName())
+                .currentAddress(user.getCurrentAddress())
+                .phone(user.getPhone())
+                .role(user.getRole().name())
                 .build();
     }
+
 }
