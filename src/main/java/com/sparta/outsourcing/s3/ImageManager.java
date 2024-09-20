77053dd91e22ab241662582ef8c101fc6b6524ce
Jay3029 <lttleleepp@gmail.com>
2024-09-20 16:46:21 +0900

    add: s3를 이용한 이미지 매니저 임시저장

---
+++ src/main/java/com/sparta/outsourcing/s3/ImageManager.java
@@ -0,0 +1,86 @@
+package com.sparta.outsourcing.s3;
+
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.IOUtils;
+import com.sparta.outsourcing.jwt.ErrorCode;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class ImageManager {
+    public String upload(MultipartFile image) {
+        if(image.isEmpty() || Objects.isNull(image.getOriginalFilename())) {
+            throw new AmazonS3Exception("not found image");
+        }
+
+        return this.uploadImage(image);
+    }
+
+    public String uploadImage(MultipartFile image) {
+        this.validateImageFileException(image.getOriginalFilename());
+        try {
+            return this.uploadImageToS3(image);
+        } catch (IOException e) {
+            throw new AmazonS3Exception("could not upload image", e);
+        }
+    }
+
+    public void validateImageFileException(String filename) {
+        int lastDotIndex = filename.lastIndexOf('.');
+        if(lastDotIndex == -1) {
+            throw new AmazonS3Exception("not found image");
+        }
+
+        String extension = filename.substring(lastDotIndex + 1).toLowerCase();
+
+        List<String> allowedExtentionList = Arrays.asList("jpg", "png", "jpeg", "gif");
+
+        if(!allowedExtentionList.contains(extension)) {
+            throw new AmazonS3Exception("not a valid image file");
+        }
+    }
+
+    private String uploadImageToS3(MultipartFile image) throws IOException {
+        String originalFilename = image.getOriginalFilename(); //원본 파일 명
+        String extention = originalFilename.substring(originalFilename.lastIndexOf(".")); //확장자 명
+
+        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + originalFilename; //변경된 파일 명
+
+        InputStream is = image.getInputStream();
+        byte[] bytes = IOUtils.toByteArray(is); //image를 byte[]로 변환
+
+        ObjectMetadata metadata = new ObjectMetadata(); //metadata 생성
+        metadata.setContentType("image/" + extention);
+        metadata.setContentLength(bytes.length);
+
+        //S3에 요청할 때 사용할 byteInputStream 생성
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+        try{
+            //S3로 putObject 할 때 사용할 요청 객체
+            //생성자 : bucket 이름, 파일 명, byteInputStream, metadata
+            PutObjectRequest putObjectRequest =
+                    new PutObjectRequest(bucketName, s3FileName, byteArrayInputStream, metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead);
+
+            //실제로 S3에 이미지 데이터를 넣는 부분이다.
+            amazonS3.putObject(putObjectRequest); // put image to S3
+        }catch (Exception e){
+            throw new S3Exception(ErrorCode.PUT_OBJECT_EXCEPTION);
+        }finally {
+            byteArrayInputStream.close();
+            is.close();
+        }
+
+        return amazonS3.getUrl(bucketName, s3FileName).toString();
+    }
+}
