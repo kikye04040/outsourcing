4e3cbc56f2e678498f18f76da76dafa863a00952
EtherXion <etel0103@gmail.com>
2024-09-21 16:10:53 +0900

    fix 필요 없는 부분 삭제

--- src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateRequestDto.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor // 테스트코드 작성 중 발견 이후 확인 할 것
 public class MenuCreateRequestDto {
-
-    private Long storeId;
 
     private String name;
     private String description;
