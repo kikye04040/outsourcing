ebbff9a58449cfa0d1b865249d8e09788367d5a5
EtherXion <etel0103@gmail.com>
2024-09-21 15:46:29 +0900

    fix 생성자 추가

--- src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateRequestDto.java
@@ -1,9 +1,11 @@
 package com.sparta.outsourcing.domain.menu.dto.request;
 
 import com.sparta.outsourcing.domain.stores.entity.Stores;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor // 테스트코드 작성 중 발견 이후 확인 할 것
 public class MenuCreateRequestDto {
 
     private Long storeId;
