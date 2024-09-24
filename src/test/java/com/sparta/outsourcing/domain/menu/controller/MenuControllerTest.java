fee5d522baa4ea2bf0ab68eb4652d2e20defd1b3
EtherXion <etel0103@gmail.com>
2024-09-24 18:35:16 +0900

    fix 메뉴 컨트롤러 테스트 미완

--- src/test/java/com/sparta/outsourcing/domain/menu/controller/MenuControllerTest.java
+++ src/test/java/com/sparta/outsourcing/domain/menu/controller/MenuControllerTest.java
@@ -1,6 +1,7 @@
 package com.sparta.outsourcing.domain.menu.controller;
 
 import com.sparta.outsourcing.domain.menu.dto.request.MenuCreateRequestDto;
+import com.sparta.outsourcing.domain.menu.dto.request.MenuUpdateRequestDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuResponseDto;
 import com.sparta.outsourcing.domain.menu.entity.Menu;
 import com.sparta.outsourcing.domain.menu.service.MenuService;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -108,32 +110,73 @@ public class MenuControllerTest {
 
     @Test
     @DisplayName("메뉴 조회 성공")
-    void getMenu() {
+    void getMenus() {
+        // given
+        Long storeId = 1L;
+        List<MenuResponseDto> menuResponseDtoList = List.of(
+                new MenuResponseDto("이미지 url", "메뉴 1", "설명 1", 12000),
+                new MenuResponseDto("이미지 url", "메뉴 2", "설명 2", 15000)
+        );
 
-        List<MenuResponseDto> menuResponseDtoList = new ArrayList<>();
+        when(menuService.getMenus(storeId)).thenReturn(menuResponseDtoList);
 
+        // when
+        ResponseEntity<List<MenuResponseDto>> response = menuController.getMenus(storeId);
 
+        // then
+        assertNotNull(response.getBody());
+        assertEquals(2, response.getBody().size());
+        verify(menuService, times(1)).getMenus(storeId);
     }
-
 
     @Test
     @DisplayName("메뉴 수정 성공")
     void updateMenu() {
+        // given
         Long storeId = 1L;
+        Long menuId = 1L;
+        MenuUpdateRequestDto menuUpdateRequestDto = new MenuUpdateRequestDto(
+                "업데이트된 이미지 url",
+                "업데이트된 이름",
+                "업데이트된 설명",
+                13000
+        );
+        MenuResponseDto menuResponseDto = new MenuResponseDto(
+                menuUpdateRequestDto.getMenuPictureUrl(),
+                menuUpdateRequestDto.getName(),
+                menuUpdateRequestDto.getDescription(),
+                menuUpdateRequestDto.getPrice()
+        );
 
+        when(menuService.updateMenu(eq(storeId), eq(menuId), any(MenuUpdateRequestDto.class), any(CustomUserDetails.class)))
+                .thenReturn(menuResponseDto);
 
+        // when
+        ResponseEntity<MenuResponseDto> response = menuController.updateMenu(
+                storeId, menuId, menuUpdateRequestDto, userDetails);
 
+        // then
+        assertNotNull(response.getBody());
+        assertEquals(menuResponseDto, response.getBody());
+        verify(menuService, times(1)).updateMenu(
+                storeId, menuId, menuUpdateRequestDto, userDetails);
     }
-
 
     @Test
     @DisplayName("메뉴 삭제 성공")
     void deleteMenu() {
+        // given
         Long storeId = 1L;
+        Long menuId = 1L;
 
+        // when
+        menuController.deleteMenu(storeId, menuId, userDetails);
 
-
+        // then
+        verify(menuService, times(1)).deleteMenu(
+                storeId, menuId, userDetails);
     }
+
 
 
 }
