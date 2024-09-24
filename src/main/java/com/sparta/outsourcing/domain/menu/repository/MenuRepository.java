de901d6af45be808b74fcb6925cfaa6b5be246fb
Jay3029 <lttleleepp@gmail.com>
2024-09-24 15:30:43 +0900

    add: search 구현, 아직 미시험

--- src/main/java/com/sparta/outsourcing/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/repository/MenuRepository.java
@@ -1,11 +1,16 @@
 package com.sparta.outsourcing.domain.menu.repository;
 
 import com.sparta.outsourcing.domain.menu.entity.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     // 메뉴 이름으로 메뉴 찾기 추가 (기혜)
     Optional<Menu> findByName(String menuName);
+    Page<Menu> findByNameContaining(String menuName, Pageable pageable);
 }
---
+++ src/main/java/com/sparta/outsourcing/domain/search/dto/SearchRequestDto.java
@@ -0,0 +1,10 @@
+package com.sparta.outsourcing.domain.search.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SearchRequestDto {
+
+    private String keyword;
+
+}
