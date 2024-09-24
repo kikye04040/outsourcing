03dbf20d8faaaf696a5b7f35bd835f4b098a9fd8
Hyemin Yang <asitwas729@gmail.com>
2024-09-25 00:03:38 +0900

    update: 특정 사용자가 좋아요한 가게 조회

--- src/main/java/com/sparta/outsourcing/domain/stores/repository/StoresLikeRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/stores/repository/StoresLikeRepository.java
@@ -5,8 +5,12 @@ import com.sparta.outsourcing.domain.stores.entity.StoresLike;
 import com.sparta.outsourcing.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoresLikeRepository extends JpaRepository<StoresLike, Long> {
     Optional<StoresLike> findByUserAndStores(User user, Stores store);
+
+    // 사용자가 좋아요한 모든 가게 조회
+    List<StoresLike> findAllByUser(User user);
 }
