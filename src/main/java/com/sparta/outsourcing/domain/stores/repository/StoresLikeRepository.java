46729d46eebc3a820aaf3ff6020184424b2a3798
Hyemin Yang <asitwas729@gmail.com>
2024-09-24 15:43:37 +0900

    add: 가게 좋아요 repository init

---
+++ src/main/java/com/sparta/outsourcing/domain/stores/repository/StoresLikeRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.outsourcing.domain.stores.repository;
+
+import com.sparta.outsourcing.domain.stores.entity.StoresLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoresLikeRepository extends JpaRepository<StoresLike, Long> {
+}
